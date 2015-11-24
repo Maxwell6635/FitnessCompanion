a59551bfc0575d40203cadfd5eaa404494cd180e
Jackson Foo Wai Hoong <jackson.foo@hexainitiative.com>
2015-11-24 10:40:27 +0800

    1) Update UI

--- app/build/generated/source/r/debug/com/github/jjobes/slidedatetimepicker/R.java
+++ app/build/generated/source/r/debug/com/github/jjobes/slidedatetimepicker/R.java
@@ -8,32 +8,32 @@ package com.github.jjobes.slidedatetimepicker;
 
 public final class R {
 	public static final class color {
-		public static final int gray_holo_dark = 0x7f0c0024;
-		public static final int gray_holo_light = 0x7f0c0025;
+		public static final int gray_holo_dark = 0x7f0c0035;
+		public static final int gray_holo_light = 0x7f0c0036;
 	}
 	public static final class drawable {
-		public static final int selection_divider = 0x7f02006c;
+		public static final int selection_divider = 0x7f0200cb;
 	}
 	public static final class id {
-		public static final int buttonHorizontalDivider = 0x7f0d015b;
-		public static final int buttonVerticalDivider = 0x7f0d015d;
-		public static final int cancelButton = 0x7f0d015c;
-		public static final int customTab = 0x7f0d0123;
-		public static final int datePicker = 0x7f0d0126;
-		public static final int okButton = 0x7f0d015e;
-		public static final int slidingTabLayout = 0x7f0d0159;
-		public static final int tabText = 0x7f0d0124;
-		public static final int timePicker = 0x7f0d012c;
-		public static final int viewPager = 0x7f0d015a;
+		public static final int buttonHorizontalDivider = 0x7f0d01ac;
+		public static final int buttonVerticalDivider = 0x7f0d01ae;
+		public static final int cancelButton = 0x7f0d01ad;
+		public static final int customTab = 0x7f0d0159;
+		public static final int datePicker = 0x7f0d015c;
+		public static final int okButton = 0x7f0d01af;
+		public static final int slidingTabLayout = 0x7f0d01aa;
+		public static final int tabText = 0x7f0d015a;
+		public static final int timePicker = 0x7f0d0162;
+		public static final int viewPager = 0x7f0d01ab;
 	}
 	public static final class layout {
-		public static final int custom_tab = 0x7f040030;
-		public static final int fragment_date = 0x7f040033;
-		public static final int fragment_time = 0x7f040035;
-		public static final int slide_date_time_picker = 0x7f040044;
+		public static final int custom_tab = 0x7f040032;
+		public static final int fragment_date = 0x7f040035;
+		public static final int fragment_time = 0x7f040037;
+		public static final int slide_date_time_picker = 0x7f040054;
 	}
 	public static final class style {
-		public static final int AppBaseTheme = 0x7f0b0002;
-		public static final int AppTheme = 0x7f0b0066;
+		public static final int AppBaseTheme = 0x7f090003;
+		public static final int AppTheme = 0x7f090078;
 	}
 }
